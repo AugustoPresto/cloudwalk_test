346f2836675d86b37aa280358af80ae130264a7c
Augusto Souza <augustodesouzacardoso@gmail.com>
2024-06-20 11:56:27 -0300

    Adding more error handling: when username param is blank or in wrong format and when status code is not 200. Also add a Rails log sentence to keep track of the errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
   def create
     username = params[:username]
+    raise "Username can't be blank" if username.blank?
+    raise "Invalid GitHub username format" unless username =~ /^[a-zA-Z0-9_-]+$/
+
     user = User.find_or_create_by(username: username)
 
     response = HTTParty.get("https://api.github.com/users/#{username}/repos")
+    raise "Failed to fetch repositories from GitHub API (status code: #{response.code})" if response.code != 200
+
     repositories = JSON.parse(response.body)
 
     repositories.each do |repo_data|
@@ -14,6 +19,7 @@ class UsersController < ApplicationController
 
     render json: { message: 'User repositories have been fetched and stored.' }, status: :ok
   rescue StandardError => e
+    Rails.logger.error("An error ocurred while fetching GitHub repositories for user #{username}: #{e.message}")
     render json: { error: e.message }, status: :unprocessable_entity
   end
 end
