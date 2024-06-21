3de8c5c1b5da7e1021275461b53881270182ca12
Augusto Souza <augustodesouzacardoso@gmail.com>
2024-06-21 17:19:24 -0300

    Add tests with VCR and more error handling errors

--- spec/jobs/fetch_github_repos_job_spec.rb
+++ spec/jobs/fetch_github_repos_job_spec.rb
@@ -1,40 +1,35 @@
 require 'rails_helper'
 require 'sidekiq/testing'
+require 'vcr'
 
 RSpec.describe FetchGithubReposJob, type: :job do
-  let(:user) { User.create!(username: 'valid-username') }
-  let(:github_response) do
-    [
-      { 'name' => 'repo1', 'stargazers_count' => 10 },
-      { 'name' => 'repo2', 'stargazers_count' => 5 }
-    ]
-  end
+  let(:user) { User.create!(username: 'AugustoPresto') }
 
-  before do
-    allow(HTTParty).to receive(:get).and_return(double(code: 200, body: github_response.to_json))
-  end
-
-  describe '#perform' do
+  describe '#perform', :vcr do
     context 'when user exists' do
       it 'fetches repositories from GitHub' do
-        expect(HTTParty).to receive(:get).with("https://api.github.com/users/#{user.username}/repos")
-        described_class.new.perform(user.username)
+        VCR.use_cassette('github_fetch_repos') do
+          described_class.new.perform(user.username)
+        end
+        expect(user.repositories.count).to be > 0
       end
 
       it 'updates user repositories' do
-        described_class.new.perform(user.username)
-        expect(user.repositories.count).to eq(2)
-        expect(user.repositories.pluck(:name)).to contain_exactly('repo1', 'repo2')
-        expect(user.repositories.find_by(name: 'repo1').stars).to eq(10)
-        expect(user.repositories.find_by(name: 'repo2').stars).to eq(5)
+        VCR.use_cassette('github_fetch_repos') do
+          described_class.new.perform(user.username)
+        end
+        repo = user.repositories.find_by(name: 'cloudwalk_test')
+        expect(repo).to be_present
+        expect(repo.stars).to be > 0
       end
 
       it 'removes deleted repositories' do
         user.repositories.create!(name: 'repo1', stars: 0)
         user.repositories.create!(name: 'repo3', stars: 0)
-        described_class.new.perform(user.username)
-        expect(user.repositories.count).to eq(2)
-        expect(user.repositories.pluck(:name)).to contain_exactly('repo1', 'repo2')
+        VCR.use_cassette('github_fetch_repos') do
+          described_class.new.perform(user.username)
+        end
+        expect(user.repositories.where(name: 'repo3')).to be_empty
       end
     end
 
@@ -44,23 +39,27 @@ RSpec.describe FetchGithubReposJob, type: :job do
       end
     end
 
-    context 'when GitHub API returns an error' do
-      before do
-        allow(HTTParty).to receive(:get).and_return(double(code: 404, body: ''))
-      end
-
-      it 'raises an error' do
-        expect { described_class.new.perform(user.username) }.to raise_error('Failed to fetch repositories from GitHub API (status code: 404)')
+    context 'when unexpected error occurs' do
+      it 'logs the error' do
+        allow(HTTParty).to receive(:get).and_raise(StandardError, 'Unexpected error')
+        expect(Rails.logger).to receive(:error).with("An error occurred while fetching GitHub repositories for user #{user.username}: Unexpected error")
+        described_class.new.perform(user.username)
       end
     end
 
-    context 'when unexpected error occurs' do
-      before do
-        allow(HTTParty).to receive(:get).and_raise(StandardError, 'Unexpected error')
+    context 'when a timeout error occurs' do
+      it 'logs the timeout error' do
+        allow(HTTParty).to receive(:get).and_raise(Net::ReadTimeout)
+        expect(Rails.logger).to receive(:error).with("An error occurred while fetching GitHub repositories for user #{user.username}: Net::ReadTimeout")
+        described_class.new.perform(user.username)
       end
+    end
 
-      it 'logs the error' do
-        expect(Rails.logger).to receive(:error).with("An error occurred while fetching GitHub repositories for user #{user.username}: Unexpected error")
+    context 'when JSON parsing error occurs' do
+      it 'logs the JSON parsing error' do
+        allow(HTTParty).to receive(:get).and_return(double(code: 200, body: 'invalid json'))
+        allow(JSON).to receive(:parse).and_raise(JSON::ParserError, 'unexpected token')
+        expect(Rails.logger).to receive(:error).with("An error occurred while fetching GitHub repositories for user #{user.username}: unexpected token")
         described_class.new.perform(user.username)
       end
     end
