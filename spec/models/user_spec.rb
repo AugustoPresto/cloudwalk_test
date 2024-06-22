5888b66e0e9729cbc565c194c69591580f86da24
Augusto Souza <augustodesouzacardoso@gmail.com>
2024-06-22 18:21:06 -0300

    Rubocop in test files

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,45 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { described_class.new(username: "ValidUsername") }
+  subject { described_class.new(username: 'ValidUsername') }
 
-  describe "Validations" do
-    it "is valid with valid attributes" do
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
       expect(subject).to be_valid
     end
 
-    it "is not valid without a username" do
+    it 'is not valid without a username' do
       subject.username = nil
       expect(subject).to_not be_valid
     end
 
-    it "is not valid with a blank string" do
-      subject.username = ""
+    it 'is not valid with a blank string' do
+      subject.username = ''
       expect(subject).to_not be_valid
     end
 
-    it "is not valid with a duplicate username" do
-      described_class.create!(username: "ValidUsername")
+    it 'is not valid with a duplicate username' do
+      described_class.create!(username: 'ValidUsername')
       expect(subject).to_not be_valid
     end
 
-    it "is not valid with a username longer than 39 characters" do
-      subject.username = "a" * 40
+    it 'is not valid with a username longer than 39 characters' do
+      subject.username = 'a' * 40
       expect(subject).to_not be_valid
     end
 
-    it "is valid with a username starting with a dash" do
-      subject.username = "-ValidUsername"
+    it 'is valid with a username starting with a dash' do
+      subject.username = '-ValidUsername'
       expect(subject).to be_valid
     end
 
-    it "is valid with a username containing letters, numbers, underscores, and dashes" do
-      subject.username = "Valid-Username_123"
+    it 'is valid with a username containing letters, numbers, underscores, and dashes' do
+      subject.username = 'Valid-Username_123'
       expect(subject).to be_valid
     end
 
-    it "is not valid with a username containing invalid characters" do
-      subject.username = "Invalid@Username"
+    it 'is not valid with a username containing invalid characters' do
+      subject.username = 'Invalid@Username'
       expect(subject).to_not be_valid
     end
   end
